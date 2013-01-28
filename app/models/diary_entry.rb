2e7ae1e63251f71f17a5f43f2ae1ef257b2e6df3
naomimatsuura <matsuura.naomi@gmail.com>
2013-01-28 18:02:38 -0500

    test before Jan 28 class

--- app/models/diary_entry.rb
+++ app/models/diary_entry.rb
@@ -1,2 +1,3 @@
 class DiaryEntry < ActiveRecord::Base
+	attr_accessible :name, :description, :user_name, :mood
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122002938) do
+ActiveRecord::Schema.define(:version => 20130125011529) do
 
   create_table "diary_entries", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130122002938) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "user_name"
+    t.string   "mood"
   end
 
 end
